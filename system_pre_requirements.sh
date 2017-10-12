dfec5501ab4e28dd6bbafa19eb03e2f1cbcbdf4a
anandreddyr11 <anandreddyr11@gmail.com>
2017-10-12 15:17:40 +0530

    Create system_pre_requirements.sh
    general requirements added here.

---
+++ system_pre_requirements.sh
@@ -0,0 +1,32 @@
+# Update the Syetem 
+  yum update -y
+ 
+# Install Wget 
+  yum install wget -y
+  
+# Install Java
+  yum install java -y
+  
+# Install Zip
+  yum install zip -y
+  
+# Install apache 
+  yum install httpd -y
+  
+# Start service Apache
+  service httpd start
+  
+# Status of Apache
+  service httpd status
+  
+# get Jenkins Repository
+  wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+  
+# Rpm used
+  rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
+  
+# Install jenkins
+  yum install jenkins -y
+  
+# Service start jenkins
+  systemctl start jenkins
